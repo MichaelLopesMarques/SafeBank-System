da372580e6af52d70d4674db6eec26c900277f93
MichaelLopesMarques <michael.marques.1458@gmail.com>
2026-02-23 15:09:34 +0100

    Regeln hinzugefügt in deposit/withdraw Methoden

--- src/main/java/com/safebank/bank_api/service/BankAccountService.java
+++ src/main/java/com/safebank/bank_api/service/BankAccountService.java
@@ -2,6 +2,8 @@ package com.safebank.bank_api.service;
 
 import com.safebank.bank_api.domain.BankAccount;
 import com.safebank.bank_api.exception.AccountNotFoundException;
+import com.safebank.bank_api.exception.InsufficientBalanceException;
+import com.safebank.bank_api.exception.InvalidAmountException;
 import com.safebank.bank_api.repository.BankAccountRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -26,6 +28,10 @@ public class BankAccountService {
     }
 
     public BankAccount deposit(String accountId, BigDecimal amount){
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0){
+            throw new InvalidAmountException("Deposit amount must be greater than zero");
+        }
+
         BankAccount account = repository.findById(accountId)
                 .orElseThrow(() -> new AccountNotFoundException("Account not found: " + accountId));
 
@@ -34,8 +40,16 @@ public class BankAccountService {
     }
 
     public BankAccount withdraw(String accountId, BigDecimal amount){
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0){
+            throw new InvalidAmountException("Deposit amount must be greater than zero");
+        }
+
         BankAccount account = repository.findById(accountId)
                 .orElseThrow(() -> new AccountNotFoundException("Account not found: " + accountId));
+
+        if (account.getBalance().compareTo(amount) < 0){
+            throw new InsufficientBalanceException("Balance not enough");
+        }
 
         account.withdraw(amount);
         return account;
