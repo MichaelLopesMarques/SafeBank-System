a5f9696c6efcae8ff17e9644d71967bb8b5a1b35
MichaelLopesMarques <michael.marques.1458@gmail.com>
2026-03-02 13:49:40 +0100

    Deposit Testcase

--- src/test/java/com/safebank/bank_api/controller/BankAccountControllerIT.java
+++ src/test/java/com/safebank/bank_api/controller/BankAccountControllerIT.java
@@ -2,12 +2,15 @@ package com.safebank.bank_api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.safebank.bank_api.dto.CreateAccountRequest;
+import com.safebank.bank_api.dto.DepositRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -52,5 +55,34 @@ class BankAccountControllerIT {
                 .andExpect(status().isConflict());
     }
 
+    @Test
+    void shouldDepositSuccessfully() throws Exception{
+        String createJson = """
+        {
+          "id": "AC-DE-2026-01",
+          "owner": "Peter Parker"
+        }
+        """;
+
+        mockMvc.perform(post("/accounts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createJson))
+                        .andExpect(status().isCreated());
+
+        String depositJson = """
+        {
+          "amount": 100
+        }
+        """;
+
+        mockMvc.perform(post("/accounts/AC-DE-2026-01/deposit")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(depositJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("AC-DE-2026-01"))
+                .andExpect(jsonPath("$.owner").value("Peter Parker"))
+                .andExpect(jsonPath("$.balance").value(100))
+                .andExpect(jsonPath("$.locked").value(false));
+    }
 
 }
