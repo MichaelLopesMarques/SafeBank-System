6e98f2437af1a08dd2dc9ab0b017da41670a33a6
MichaelLopesMarques <michael.marques.1458@gmail.com>
2026-03-05 15:42:25 +0100

    Deleted ununsed Import

--- src/test/java/com/safebank/bank_api/controller/BankAccountControllerIT.java
+++ src/test/java/com/safebank/bank_api/controller/BankAccountControllerIT.java
@@ -1,7 +1,6 @@
 package com.safebank.bank_api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.safebank.bank_api.dto.CreateAccountRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
